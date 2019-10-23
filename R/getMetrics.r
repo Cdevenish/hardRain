4713c54daad11150fba7e9fa50bb1ea0481bb497
Christian Devenish <Cdevenish@users.noreply.github.com>
2019-10-23 15:55:56 +0100

    Update getMetrics.r

--- R/getMetrics.r
+++ R/getMetrics.r
@@ -63,133 +63,147 @@ getMetrics <- function(wav, freqLo = c(0.6, 4.4), freqHi = c(1.2,5.6), t.step = 
   # catch read wav errors, with try and record them here: Only when wav is filenames
   # if(class(wav) == "character") tryError <- vector(mode = "logical", length = length(wav))
 
-  if(parallel){
+  ### if wav object, then read in here directly... and no parallel options
 
-    # library(parallel) # in base R.. so ok just to ::
+  if(class(wav) == "Wave") { ### include package environment as well here...
 
-    noCores <- parallel::detectCores() - 1
-    cl <- parallel::makeCluster(noCores)
+    b <- wav
 
-    parallel::clusterExport(cl, c("wav", "t.step", "fftw", "freqLo", "freqHi"), envir = environment())
-    parallel::clusterEvalQ(cl, {
-      library(seewave)
-      library(tuneR)
-    }
-    )
-    # appFn <- parallel::parLapply
+    ## FINISH THIS...
+    ## what happens if a mfs.lst element is NA further down.... find out, before adding this...
 
-    ## do a burn in? not sure this helps, but second time around often seems quicker... is there some caching going
-    ## on somewhere?
-
-    # if(burnIn){
-    #   print("Doing burn in...")
-    #   mfs.tmp <- parallel::parLapply(cl, wav[1], function(x) {
+    # b <- try(tuneR::readWave(x), silent = T)
     #
-    #     b <- tuneR::readWave(x, header = T) # read in audiofile
-    #     if(b$samples/b$sample.rate > 15) to <- 10 else to <- floor(b$samples/b$sample.rate)
-    #     b <- tuneR::readWave(x, from = 1, to = to, units = "seconds") # read in audiofile
-    #     f <- as.numeric(b@samp.rate)
-    #     # get freq spectrum
-    #     fs <- seewave::spectro(b, wl = 512, wn="rectangle", fftw=fftw, plot=F, dB = NULL) #
-    #     # str(fs)
-    #
-    #   })
-    #   rm(mfs.tmp)
-    # }
+    # tryErr <- inherits(b, "try-error")
+    # tryError[which(wav == x)] <- tryErr
 
-    # is it quicker to export all wav objects
+    f <- as.numeric(b@samp.rate)
 
-    mfs.lst <- parallel::parLapply(cl, wav, function(x) {
+    # get wl from t.step (a few ms will probably be left unprocessed at end of each file with step)
+    if(!is.null(t.step)) wl <- t.step*f else wl <- length(b)
+    wl <- wl - wl%%2 # make sure it's even
 
-      #if(!parallel) setTxtProgressBar(pb, which(x == wav))
+    # get freq spectrum
+    fs <- seewave::spectro(b, wl = wl, wn="rectangle", fftw=fftw, plot=F, dB = NULL) # wl = 512 default, ...
+    # str(fs)
+    # with dB = NULL, then this gives a ^2 already, even if dBref is NULL
+    # 'dB' argument computes 20*log10(x) where x is the FFT, which is equivalent to 10*log10(x^2)
+    # why does ... work wiht parallel, when dB isn't exported?? because the value is given.. whereas wav is not.
+    # p 235 of Sueur book for amplitude normalisation.
 
-      if(class(wav) == "Wave"){b <- wav} else {
+    # take psd scores for each rain frequency window in khz
+    mfs.lst <- list(mapply(function(lo,hi) fs$amp[fs$freq > lo & fs$freq < hi, ,drop = F],
+           freqLo, freqHi, SIMPLIFY = F))
+    # str(tmp2)
 
-        # read in audiofile
-        b <- tuneR::readWave(x)
-
-        ## FINISH THIS...
-        ## what happens if a mfs.lst element is NA further down.... find out, before adding this...
-
-        # b <- try(tuneR::readWave(x), silent = T)
-        #
-        # tryErr <- inherits(b, "try-error")
-        # tryError[which(wav == x)] <- tryErr
-
-      }
-
-      ## hacky way....
-      # if(class(wav) == "Wave" & tryErr) NA else {
-
-      f <- as.numeric(b@samp.rate)
-
-      # get wl from t.step (a few ms will probably be left unprocessed at end of each file with step)
-      if(!is.null(t.step)) wl <- t.step*f else wl <- length(b)
-      wl <- wl - wl%%2 # make sure it's even
-
-      # get freq spectrum
-      fs <- seewave::spectro(b, wl = wl, wn="rectangle", fftw=fftw, plot=F, dB = NULL) # wl = 512 default, ...
-      # str(fs)
-      # with dB = NULL, then this gives a ^2 already, even if dBref is NULL
-      # 'dB' argument computes 20*log10(x) where x is the FFT, which is equivalent to 10*log10(x^2)
-      # why does ... work wiht parallel, when dB isn't exported?? because the value is given.. whereas wav is not.
-      # p 235 of Sueur book for amplitude normalisation.
-
-      # take psd scores for each rain frequency window in khz
-      mapply(function(lo,hi) fs$amp[fs$freq > lo & fs$freq < hi, ,drop = F],
-             freqLo, freqHi, SIMPLIFY = F)
-      # str(tmp2)
-
-      #} # end of else if not a try error
-
-    })
-
-    parallel::stopCluster(cl)
+    #} # end of else if not a try error
 
 
   } else {
 
-    # appFn <- lapply
-    pb <- txtProgressBar(min = 0, max = length(wav), style = 3) # length of all wavs?
+    if(parallel){
 
-    mfs.lst <- lapply(wav, function(x) {
+      # library(parallel) # in base R.. so ok just to ::
 
-      setTxtProgressBar(pb, which(x == wav))
+      noCores <- parallel::detectCores() - 1
+      cl <- parallel::makeCluster(noCores)
 
-      if(class(wav) == "Wave"){b <- wav} else {
+      parallel::clusterExport(cl, c("wav", "t.step", "fftw", "freqLo", "freqHi"), envir = environment())
+      parallel::clusterEvalQ(cl, {
+        library(seewave)
+        library(tuneR)
+      }
+      )
+      # appFn <- parallel::parLapply
+
+      ## do a burn in? not sure this helps, but second time around often seems quicker... is there some caching going
+      ## on somewhere?
+
+      # if(burnIn){
+      #   print("Doing burn in...")
+      #   mfs.tmp <- parallel::parLapply(cl, wav[1], function(x) {
+      #
+      #     b <- tuneR::readWave(x, header = T) # read in audiofile
+      #     if(b$samples/b$sample.rate > 15) to <- 10 else to <- floor(b$samples/b$sample.rate)
+      #     b <- tuneR::readWave(x, from = 1, to = to, units = "seconds") # read in audiofile
+      #     f <- as.numeric(b@samp.rate)
+      #     # get freq spectrum
+      #     fs <- seewave::spectro(b, wl = 512, wn="rectangle", fftw=fftw, plot=F, dB = NULL) #
+      #     # str(fs)
+      #
+      #   })
+      #   rm(mfs.tmp)
+      # }
+
+      # is it quicker to export all wav objects
+
+      mfs.lst <- parallel::parLapply(cl, wav, function(x) {
+
+        #if(!parallel) setTxtProgressBar(pb, which(x == wav))
 
         # read in audiofile
         b <- tuneR::readWave(x)
 
-        # FINISH THIS....
-        # b <- try(tuneR::readWave(x), silent = T)
-        #
-        # tryErr <- inherits(b, "try-error")
-        # tryError[which(wav == x)] <- tryErr
+        f <- as.numeric(b@samp.rate)
 
-      }
+        # get wl from t.step (a few ms will probably be left unprocessed at end of each file with step)
+        if(!is.null(t.step)) wl <- t.step*f else wl <- length(b)
+        wl <- wl - wl%%2 # make sure it's even
+
+        # get freq spectrum
+        fs <- seewave::spectro(b, wl = wl, wn="rectangle", fftw=fftw, plot=F, dB = NULL) # wl = 512 default, ...
+        # str(fs)
+        # with dB = NULL, then this gives a ^2 already, even if dBref is NULL
+        # 'dB' argument computes 20*log10(x) where x is the FFT, which is equivalent to 10*log10(x^2)
+        # why does ... work wiht parallel, when dB isn't exported?? because the value is given.. whereas wav is not.
+        # p 235 of Sueur book for amplitude normalisation.
+
+        # take psd scores for each rain frequency window in khz
+        mapply(function(lo,hi) fs$amp[fs$freq > lo & fs$freq < hi, ,drop = F],
+               freqLo, freqHi, SIMPLIFY = F)
+        # str(tmp2)
+
+        #} # end of else if not a try error
+
+      })
+
+      parallel::stopCluster(cl)
 
 
+    } else {
 
-      f <- as.numeric(b@samp.rate)
+      # appFn <- lapply
+      pb <- txtProgressBar(min = 0, max = length(wav), style = 3) # length of all wavs?
 
-      # get wl from t.step (a few ms will probably be left unprocessed at end of each file with step)
-      if(!is.null(t.step)) wl <- t.step*f else wl <- length(b)
-      wl <- wl - wl%%2 # make sure it's even
 
-      # get freq spectrum
-      fs <- seewave::spectro(b, wl = wl, wn="rectangle", fftw=fftw, plot=F, dB = NULL) #, ...  wl = 512 default
-      # str(fs)
-      # with dB = NULL, then this gives a ^2 already, even if dBref is NULL
-      # 'dB' argument computes 20*log10(x) where x is the FFT, which is equivalent to 10*log10(x^2)
+      mfs.lst <- lapply(wav, function(x) {
 
-      # take psd scores for each rain frequency window in khz
-      mapply(function(lo,hi) fs$amp[fs$freq > lo & fs$freq < hi, ,drop = F],
-           freqLo, freqHi, SIMPLIFY = F)
-      #str(psd.freq)
-  })
+        setTxtProgressBar(pb, which(x == wav))
 
-    close(pb)
+        # read in audiofile
+        b <- tuneR::readWave(x)
+
+        f <- as.numeric(b@samp.rate)
+
+        # get wl from t.step (a few ms will probably be left unprocessed at end of each file with step)
+        if(!is.null(t.step)) wl <- t.step*f else wl <- length(b)
+        wl <- wl - wl%%2 # make sure it's even
+
+        # get freq spectrum
+        fs <- seewave::spectro(b, wl = wl, wn="rectangle", fftw=fftw, plot=F, dB = NULL) #, ...  wl = 512 default
+        # str(fs)
+        # with dB = NULL, then this gives a ^2 already, even if dBref is NULL
+        # 'dB' argument computes 20*log10(x) where x is the FFT, which is equivalent to 10*log10(x^2)
+
+        # take psd scores for each rain frequency window in khz
+        mapply(function(lo,hi) fs$amp[fs$freq > lo & fs$freq < hi, ,drop = F],
+               freqLo, freqHi, SIMPLIFY = F)
+        #str(psd.freq)
+      })
+
+      close(pb)
+    }
+
   }
 
   ## display filenames that couldn't be loaded with readwav
@@ -226,10 +240,16 @@ getMetrics <- function(wav, freqLo = c(0.6, 4.4), freqHi = c(1.2,5.6), t.step = 
   tmp <- lapply(1:2, function(x) do.call(rbind, sapply(res, function(y) y[x])))
   res2 <- do.call(cbind, tmp)
   # # head(res2)
+
+  if(class(wav)== "Wave") fnames <- 1 else {
+    fnames <- basename(wav)
+  }
+
   cNames <- apply(expand.grid("band", seq_along(freqLo), c("psd", "s2n"), KEEP.OUT.ATTRS = F),
                   1, paste0, collapse = ".")
   name.exp <- sapply(res, function(x) sapply(x, nrow)[1])
-  dimnames(res2) <- list(mapply(function(x,y) rep(x, each= y), basename(wav), name.exp), cNames)
+
+  dimnames(res2) <- list(mapply(function(x,y) rep(x, each= y), fnames, name.exp), cNames)
 
   if(!is.null(t.step)) {
 
